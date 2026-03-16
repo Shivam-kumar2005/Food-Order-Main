9b06d3a877e24966cc7fedb95c40629f30dc076f
Shivam kumar singh <kumarshivamsinghsingh1223@gmail.com>
2026-03-16 10:05:46 +0530

    feat: extract About section into component

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,14 @@ import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
 import SectionHeaders from "@/components/layout/SectionHeaders";
+import About from "@/components/layout/About";
 
 export default function Home() {
   return (
     <>
      <Hero/>
      <HomeMenu/>
-     <section className="text-center my-16">
-      <SectionHeaders subHeader={'Our Story'} mainHeader={'About Us'}/>
-      <div className="max-w-2xl mx-auto mt-4 text-gray-500 flex flex-col gap-4">
-      <p >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
-      </div>
-      
-     </section>
+     <About />
      <section className="text-center my-16">
       <SectionHeaders subHeader={"Don't hesitate"} mainHeader={'Contact Us'}/>
       <div className="mt-8">
---
+++ src/components/layout/About.js
@@ -0,0 +1,17 @@
+import SectionHeaders from "@/components/layout/SectionHeaders";
+
+export default function About() {
+  return (
+    <section className="text-center my-16" id="about">
+      <SectionHeaders subHeader={'Our Story'} mainHeader={'About Us'}/>
+      <div className="max-w-2xl mx-auto mt-4 text-gray-500 flex flex-col gap-4">
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+        </p>
+      </div>
+    </section>
+  );
+}
